3cb8ff18f191f75c7382bdc7db8041b6e392400b
Selam <84750917+sel6@users.noreply.github.com>
2021-11-09 13:54:25 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,19 @@ class BaseModel:
     def __init__(self, id, created_at, updated_at):
         """unique IDs"""
         self.id = str(uuid.uuid4())
-        self.created_at = date.today()
+        self.created_at = datetime.today()
         self.updated_at = self.created_at
     
     def __str__(self):
         return"[{}]({}){}.format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+         """updates the public instance attribute updated_at with the current datetime"""
+         self.updated_at = datetime.today()
+         
+        
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        for key, value in self.__dict__.items():
+            repr(self.__dict__)
+            
