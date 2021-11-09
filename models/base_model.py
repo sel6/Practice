d43e65f62705c6aa293944705043f05e5f478a82
Selam <84750917+sel6@users.noreply.github.com>
2021-11-09 07:07:53 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import uuid
+
+class BaseModel:
+    """ common attribute, methods
+    for other classes"""
+    def __init__(self, id):
+        """unique IDs"""
+        self.id=uuid.uuid(4)
+ 
