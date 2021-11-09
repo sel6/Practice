67a48f3bdeff2b261a7bde8304ed4ade7661d893
Selam <84750917+sel6@users.noreply.github.com>
2021-11-09 12:26:30 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,9 @@ class BaseModel:
     for other classes"""
     def __init__(self, id, created_at, updated_at):
         """unique IDs"""
-        self.id=id
-        self.created_at=created_at
-        self.updated_at=updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = date.today()
+        self.updated_at = self.created_at
+    
+    def __str__(self):
+        print(.format( ,self.id, self.__dict__)
