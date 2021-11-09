79d7083dae0c23908808ba171584ec491925ae41
Selam <84750917+sel6@users.noreply.github.com>
2021-11-09 07:21:45 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,8 @@ import uuid
 class BaseModel:
     """ common attribute, methods
     for other classes"""
-    def __init__(self, id):
+    def __init__(self, id, created_at, updated_at):
         """unique IDs"""
         self.id=str(uuid.uuid(4))
- 
+        self.created_at=
+        self.updated_at=
