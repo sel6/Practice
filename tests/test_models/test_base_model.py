b06dedb25a320b37547ccf21999852121b835aea
Selam <84750917+sel6@users.noreply.github.com>
2021-11-10 14:17:22 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 import unittest
+import model
+from datetime import date
+
+class TestBaseModel:
+  
+  
+  def test___init__(self):
+    isinstance(self.id, uuid.uuid4())
+    assertIsInstance(self.id, uuid.uuid4())
+    isinstance(self.id, str)
+    assertIsInstance(self.id, str)
+    assertIsEqual(created_at, datetime.today())
+    assertIsEqual(updated_at, created_at)
+    
+  def test___str__(self):
+    
+    
+    
+  def test_save(self):
+    
+   
+  def test_to_dict(self):
