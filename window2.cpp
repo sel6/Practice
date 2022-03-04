5fd929c73adfb33f6bdb2f0021df739a1399d612
Selam <84750917+sel6@users.noreply.github.com>
2022-03-04 13:14:26 -0800

    Update window2.cpp

--- window2.cpp
+++ window2.cpp
@@ -1,40 +1,20 @@
-// Example program:
-// Using SDL2 to create an application window
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
 
-#include "SDL.h"
-#include <stdio.h>
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char* argv[]) {
+	// returns zero on success else non-zero
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		printf("error initializing SDL: %s\n", SDL_GetError());
+	}
+	SDL_Window* win = SDL_CreateWindow("GAME",
+									SDL_WINDOWPOS_CENTERED,
+									SDL_WINDOWPOS_CENTERED,
+									1000, 1000, 0);
+	while (1)
+		;
 
-    SDL_Window *window;                    // Declare a pointer
-
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
-
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    // The window is open: could enter program loop here (see SDL_PollEvent())
-
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-
-    // Clean up
-    SDL_Quit();
-    return 0;
+	return 0;
 }
